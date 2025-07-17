36f0eb5613f57fee82502dd12b665dcc679a0616
Rayn <greenbellpaper13@gmail.com>
2025-07-16 23:19:28 -0400

    new

--- Data.py
+++ Data.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:223c78c6736da04a0f2cc2cfdd2df566dc601b88b1baeb3fc530fcb120b0bac3
-size 54792
+oid sha256:eaecb2b28739f5a3a982cd30a1bfebafb8fd3faf4ca36f3b3d22935cf7f64421
+size 54721
--- ImageWROp.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:504ece81ca53ff17eb3e5a797377347e38a2385e46b10195a0dd497640c1bdb9
-size 2085
