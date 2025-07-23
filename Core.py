f65d6643a0a73aa12ee74c1b14234b4edfed9068
Rayn <greenbellpaper13@gmail.com>
2025-07-22 22:46:57 -0400

    new

--- Core.py
+++ Core.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47371000adf04d418a64a7d4d9da5409c1c24366ebac985ae05f412e9f11b7d6
-size 73521
+oid sha256:16e7680b8173be876489c9ee102041dda706f1609d20fc0f4cdc64d5686e1f52
+size 73541
